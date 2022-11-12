26c146c428238ff2b8eef1f8b9ed0f5a0bc32b69
Arindam-Sarkar <arindamwork7@gmail.com>
2022-11-12 21:34:17 +0530

    Additions made to SingleDayDetails page

--- mern-google-calender-clone-client/src/singeDay/SingleDayDetails.jsx
+++ mern-google-calender-clone-client/src/singeDay/SingleDayDetails.jsx
@@ -17,7 +17,9 @@ const taskColor = [
 
 
 const SingleDayDetails = () => {
-  const [singleDayColor, setSingleDayColor] = useState([false, false, false, false, false, false])
+  const [singleDayColor, setSingleDayColor] = useState([true, false, false, false, false, false])
+  const [singleDayTitle, setSingleDayTitle] = useState("")
+  const [singleDayDesc, setSingleDayDesc] = useState("")
 
   const dayColorClickHandler = (index) => {
     let singleDayColorTmp = singleDayColor.map((elemet, i) => {
@@ -28,22 +30,18 @@ const SingleDayDetails = () => {
         return false
       }
     })
-    console.log("singleDayColorTmp : ", singleDayColorTmp);
     setSingleDayColor(singleDayColorTmp)
   }
 
   return (
     <div className='border w-screen h-screen flex  justify-center items-center'>
 
-      <div className='md:w-[40%] md:h-[55%] border-2 border-gray-300 flex flex-col 
-      rounded-2xl bg-blue sd:w-[90%] sd:h-[40%] '>
+      <div className='md:w-[40%] md:h-[55%] border-2 border-gray-200 flex flex-col rounded-2xl w-[90%] h-[45%] '>
 
-
-
-        <div className='w-[100%] md:h-[10%]  rounded-t-2xl bg-gray-400 flex flex-col justify-center align-middle'>
+        <div className='w-[100%] md:h-[20%] h-[15%] rounded-t-2xl bg-gray-400 flex flex-col justify-center align-middle'>
           <div className=' flex flex-row  justify-between ml-7 mr-7'>
             <div>
-              <span className='text-[1.5rem] text-gray-100  '> <AiOutlineMenu /></span>
+              <span className='text-[1.5rem] text-gray-100'> <AiOutlineMenu /></span>
             </div>
 
             <div className='flex flex-row gap-4'>
@@ -59,13 +57,15 @@ const SingleDayDetails = () => {
           <input
             type="text"
             placeholder='Add Title'
-            className='w-[80%] mt-7 border-0 border-b-2 ml-[15%] outline-none 
-            border-transparent focus:outline-none focus:ring-0 text-gray-900 text-[1.5rem]' />
+            className='w-[80%] mt-7 border-0 border-b-2 ml-[16%]  outline-none 
+            border-transparent focus:outline-none focus:ring-0 text-gray-900 md:text-[1.5rem]  text-[1.2rem]'
+            onChange={e => setSingleDayTitle(e.target.value)}
+          />
         </div>
 
         <div className='flex  w-[100%] h-[80px] flex-row justify-start items-center'>
           <div className='text-[1.5rem] text-gray-900 ml-[3%]'> <MdOutlineWatchLater /> </div>
-          <div className='text-gray-900 text-[1.5rem] ml-[12%]'>Wednesday, November 16</div>
+          <div className='text-gray-900 ml-[12%] md:text-[1.5rem]  text-[1.2rem]'>Wednesday, November 16</div>
         </div>
 
 
@@ -73,9 +73,11 @@ const SingleDayDetails = () => {
           <div className='text-[1.5rem] text-gray-900 ml-[3%]'> <HiOutlineMenuAlt3 /> </div>
           <input
             type="text"
-            placeholder='Add Title'
-            className='w-[80%] mt-7 border-0 border-b-2 ml-[11%] outline-none 
-            border-transparent focus:outline-none focus:ring-0 text-gray-900 text-[1.5rem]' />
+            placeholder='Add a description'
+            className='w-[80%] mt-7 border-0 border-b-2 md:ml-[11%] ml-[9%] outline-none 
+            border-transparent focus:outline-none focus:ring-0 text-gray-900 md:text-[1.5rem]  text-[1.2rem]'
+            onChange={e => setSingleDayDesc(e.target.value)}
+          />
         </div>
 
         <div className='flex  w-[100%] h-[80px] flex-row justify-start items-center'>
@@ -101,7 +103,8 @@ const SingleDayDetails = () => {
         </div>
 
         <div className='flex  w-[100%] h-[80px] flex-row justify-end items-center'>
-          <button className='w-[110px] h-[40px] mr-[5%] rounded bg-blue-400 text-[white]'>Save</button>
+          <button className='md:w-[110px] md:h-[40px] mr-[5%] rounded bg-blue-400 text-[white]
+           w-[90px] h-[35px]'>Save</button>
         </div>
 
       </div>
