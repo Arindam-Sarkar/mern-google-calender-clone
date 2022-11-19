f67491a10a3a14f166b9d159f269f2826c33233d
Arindam-Sarkar <arindamwork7@gmail.com>
2022-11-19 05:50:22 +0530

    Added backend with jwt for basic user login,register and crud for tasks for user

---
+++ mern-google-calender-clone-server/controllers/taskController.js
@@ -0,0 +1,47 @@
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import { createErrorMsg } from '../utils/errorResponse.js'
+
+import taskModel from '../models/taskModel.js'
+
+export const createTask = async (req, res, next) => {
+  const newTask = taskModel(req.body)
+  try {
+    const savedTask = await newTask.save({ new: true })
+    res.status(200).json(savedTask)
+  } catch (error) {
+    console.log(error)
+    next(error)
+  }
+}
+
+export const updateTask = async (req, res, next) => {
+  try {
+    const updatedTasks = await taskModel.findOneAndUpdate(
+      { userId: req.params.userId, taskId: req.body.taskId }, { ...req.body }, { new: true },)
+    res.status(200).json(updatedTasks)
+  } catch (error) {
+    console.log(error)
+    next(error)
+  }
+}
+
+export const removeTask = async (req, res, next) => {
+  try {
+    const updatedTasks = await taskModel.findOneAndDelete(
+      { userId: req.params.userId, taskId: req.body.taskId }, { new: true })
+    res.status(200).json(updatedTasks)
+  } catch (error) {
+    console.log(error)
+    next(error)
+  }
+}
+
+export const getAllTasks = async (req, res, next) => {
+  try {
+    const searchedTasks = await taskModel.find({ userId: req.params.userId })
+    res.status(200).json(searchedTasks)
+  } catch (error) {
+    next(error)
+  }
+}
