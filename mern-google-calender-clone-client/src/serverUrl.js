3dc164869d28d21866f0432755255046be5d5104
Arindam-Sarkar <arindamwork7@gmail.com>
2022-12-04 20:33:43 +0530

    Changes made to connect to cyclic url

--- mern-google-calender-clone-client/src/serverUrl.js
+++ mern-google-calender-clone-client/src/serverUrl.js
@@ -1,4 +1,4 @@
-export const serverUrl = "http://localhost:8800/api"
+// export const serverUrl = "http://localhost:8800/api"
 
-// export const serverUrl = "https://fancy-plum-smock.cyclic.app/api"
+export const serverUrl = "https://cheerful-ruby-zipper.cyclic.app/api"
 
